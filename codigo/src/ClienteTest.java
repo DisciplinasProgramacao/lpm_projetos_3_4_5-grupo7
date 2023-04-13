92d718806679e3521c5f5e0e9addb4fc1a39c1f2
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-04-12 21:58:29 -0300

    estruturando classes de teste

--- codigo/src/ClienteTest.java
+++ codigo/src/ClienteTest.java
@@ -1,5 +1,16 @@
 package src;
 
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+
 public class ClienteTest {
-  
+  @Test
+  public void testaAdicionarNaLista() {
+
+  }
+
+  @Test
+  public void testaRetirarDaLista() {
+
+  }
 }
--- codigo/src/PlataformaStreamingTest.java
+++ codigo/src/PlataformaStreamingTest.java
@@ -1,5 +1,41 @@
 package src;
 
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+
 public class PlataformaStreamingTest {
-  
+  @Test
+  public void testaLogin() {
+
+  }
+
+  @Test
+  public void testaAdicionarSerie() {
+
+  }
+
+  @Test
+  public void testaAdicionarCliente() {
+
+  }
+
+  @Test
+  public void testaFiltrarPorGenero() {
+
+  }
+
+  @Test
+  public void testaFiltrarIdioma() {
+
+  }
+
+  @Test
+  public void testaFiltrarPorQtdEpisodios() {
+
+  }
+
+  @Test
+  public void testaRegistrarAudiencia() {
+    
+  }
 }
