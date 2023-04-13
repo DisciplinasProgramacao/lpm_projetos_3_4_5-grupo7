da1177337edeef2f3134ae1abd706c7c8f5592f7
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-04-12 22:11:19 -0300

    testando audiencia

--- codigo/src/SerieTest.java
+++ codigo/src/SerieTest.java
@@ -1,11 +1,17 @@
 package src;
 
-import org.junit.*;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
 public class SerieTest {
   @Test
   public void testaRegistraAudiencia() {
+    Serie serie = new Serie();
 
+    serie.registrarAudiencia();
+    serie.registrarAudiencia();
+    serie.registrarAudiencia();
+
+    Assertions.assertEquals(3, serie.getAudiencia());
   }
 }
