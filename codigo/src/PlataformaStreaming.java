c777babdf9ba1dca170d968fc6e37ea7cf909d2e
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-04-13 21:11:18 -0300

    documentando a classe plataforma

--- codigo/src/PlataformaStreaming.java
+++ codigo/src/PlataformaStreaming.java
@@ -10,8 +10,13 @@ public class PlataformaStreaming {
     private HashSet<Cliente> clientes;
     private Cliente clienteAtual;
 
+    /** 
+     * Valida as informações do usuário, efetuando assim seu login e retornando um novo cliente
+     * @param nomeUsuario
+     * @param senha
+     * @return Retorna um novo cliente
+     */
     public Cliente login(String nomeUsuario, String senha) {
-
         for (Cliente cliente : this.clientes)
             if (cliente.getSenha() == senha && cliente.getNomeUsuario() == nomeUsuario)
                 return cliente;
@@ -19,14 +24,27 @@ public class PlataformaStreaming {
         return new Cliente();
     }
 
+    /**
+     * Adiciona uma série em uma lista de séries dentro da plataforma
+     * @param serie
+     */
     public void adicionarSerie(Serie serie) {
         this.series.add(serie);
     }
 
+    /**
+     * Adiciona um cliente em uma lista de clientes dentro da plataforma
+     * @param cliente
+     */
     public void adicionarCliente(Cliente cliente) {
         this.clientes.add(cliente);
     }
 
+    /**
+     * Retorna uma lista de séries de acordo com um gênero específico
+     * @param genero
+     * @return Lista de séries de um gênero específico
+     */
     public List<Serie> filtrarPorGenero(String genero) {
         List<Serie> series = new ArrayList<Serie>();
         // for(Serie serie : this.series)
@@ -34,22 +52,44 @@ public class PlataformaStreaming {
         return series;
     }
 
+    /**
+     * Retorna uma lista de séries de acordo com o idioma informado
+     * @param idioma
+     * @return Lista de séries de um idioma específico
+     */
     public List<Serie> filtrarPorIdioma(String idioma) {
         return new ArrayList<Serie>();
     }
 
+    /** 
+     * Retorna uma lista de séries de acordo com a quantidade de episódeos informada
+     * @param idioma
+     * @return Lista de séries de uma determinada quantidade de episódeos
+     */
     public List<Serie> filtrarPorQtdEpisodios(String idioma) {
         return new ArrayList<Serie>();
     }
 
+    /**
+     * -------------------
+     * @param serie
+     */
     public void registrarAudiencia(Serie serie) {
 
     }
 
+    /**
+     * Desconecta/desloga o usuário da plataforma
+     */
     public void logoff() {
         this.clienteAtual = null;
     }
 
+    /** 
+     * O método acima retorna uma série de acordo com o nome informado
+     * @param nomeSerie
+     * @return Retorna uma série específica
+     */
     public Serie buscaSerie(String nomeSerie) {
         return new Serie();
     }
