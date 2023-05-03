89b570f587cbfba10bb4b3b8a1bd740b603f0710
Marcos-PA <mp6046969@gmail.com>
2023-05-03 20:10:39 -0300

    Atualização Main

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -2,6 +2,9 @@ package src;
 
 import java.util.Scanner;
 
+import src.Audiovisual;
+import src.Filme;
+
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,7 +19,9 @@ public class App {
             System.out.println("2 - Ver catálogo de séries");
             System.out.println("3 - Buscar Séries:");
             System.out.println("4 - Ver perfil");
-            System.out.println("5 - Sair");
+            System.out.println("5 - Adicionar Filme");
+            System.out.println("6 - Adicionar Serie");
+            System.out.println("7 - Sair");
             int opcao = scanner.nextInt();
 
             switch (opcao) {
@@ -39,12 +44,35 @@ public class App {
                     meusDados.carregarClientes();
                     break;
                 case 5:
+                    System.out.println("Adicionar Filme...");
+                    System.out.println("Digite o ID do filme");
+                    int id = scanner.nextInt();
+                    System.out.println("Digite o nome do filme:");
+                    String nome = scanner.next();
+                    System.out.println("Digite a data de lançamento:");
+                    String anoLancamento = scanner.next();
+                    System.out.println("Digite a duração do filme:");
+                    int duracao = scanner.nextInt();
+                    Filme filme = new Filme(id, nome, anoLancamento, duracao);
+                    ps.adicionarFilme(filme);
+                    ps.salvarFilme();
+                    break;
+
+                case 6:
+                    System.out.println("Adicionar Serie...");
+                    System.out.println("Digite o ID da serie");
+                    int idSerie = scanner.nextInt();
+                    System.out.println("Digite o nome da serie:");
+                    String nomeSerie = scanner.next();
+                    System.out.println("Digite a data de lançamento:");
+                    String anoLancamentoSerie = scanner.next();
+                    Serie serie = new Serie(idSerie, nomeSerie, anoLancamentoSerie);
+                    ps.adicionarSerie(serie);
+                    ps.salvarSerie();
+                    break;
+                case 7:
                     System.out.println("Saindo...");
                     sair = true;
-                    break;
-                case 999:
-                    System.out.println("Test...");
-                    ps.salvarFilme();
                     break;
                 default:
                     System.out.println("Opção inválida!");
