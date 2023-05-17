26c20c93fc51922f53d1ace60228f0d3dcb19bde
Samuel Leal <samuelmarquessousaleal@gmail.com>
2023-05-17 16:07:43 -0300

    Modularizando main

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -3,73 +3,73 @@ import src.*;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        PlataformaStreaming ps = new PlataformaStreaming();
-        DadosService meusDados = new DadosService(ps);
-        boolean sair = false;
+    static Scanner scanner = new Scanner(System.in);
 
-        while (!sair) {
-            System.out.println("Bem-vindo ao StreamingApp!");
-            System.out.println("Escolha uma opção:");
-            System.out.println("1 - Ver catálogo de filmes");
-            System.out.println("2 - Ver catálogo de séries");
-            System.out.println("3 - Buscar Séries:");
-            System.out.println("4 - Ver perfil");
-            System.out.println("5 - Adicionar Filme");
-            System.out.println("6 - Adicionar src.Serie");
-            System.out.println("7 - Assistir");
-            System.out.println("8 - Sair");
-            int opcao = scanner.nextInt();
+    public static void limparTela() {
+        System.out.print("\033[H\033[2J");
+        
+    }
 
-            switch (opcao) {
-                case 1:
-                    System.out.println("Catálogo de filmes:");
-                    meusDados.carregarFilmes();
-                    break;
-                case 2:
-                    System.out.println("Catálogo de séries:");
-                    meusDados.carregarSeries();
-                    break;
-                case 3:
-                    System.out.println("Buscar Séries:");
-                    System.out.println("Digite o nome do titulo:");
-                    String audiovisual = scanner.next();
-                    System.out.println(ps.buscarAudiovisual(audiovisual));
-                    break;
-                case 4:
-                    System.out.println("Perfil:");
-                    meusDados.carregarClientes();
-                    break;
-                case 5:
-                    System.out.println("Adicionar Filme...");
-                    System.out.println("Digite o ID do filme");
-                    int id = scanner.nextInt();
-                    System.out.println("Digite o nome do filme:");
-                    String nome = scanner.next();
-                    System.out.println("Digite a data de lançamento:");
-                    String anoLancamento = scanner.next();
-                    System.out.println("Digite a duração do filme:");
-                    int duracao = scanner.nextInt();
-                    Filme filme = new Filme(id, nome, anoLancamento, duracao);
-                    ps.adicionarFilme(filme);
-                    ps.salvarFilme();
-                    break;
+    static void pausa() {
+        System.out.println("Enter para continuar.");
+        scanner.nextLine();
+    }
 
-                case 6:
-                    System.out.println("Adicionar src.Serie...");
-                    System.out.println("Digite o ID da serie");
-                    int idSerie = scanner.nextInt();
-                    System.out.println("Digite o nome da serie:");
-                    String nomeSerie = scanner.next();
-                    System.out.println("Digite a data de lançamento:");
-                    String anoLancamentoSerie = scanner.next();
-                    Serie serie = new Serie(idSerie, nomeSerie, anoLancamentoSerie);
-                    ps.adicionarSerie(serie);
-                    ps.salvarSerie();
-                    break;
-                case 7:
-                    System.out.println("Oque deseja ver ? 1 -> Filme 2 -> serie");
+    public static int menu() {
+        limparTela();
+        System.out.println("Bem-vindo ao StreamingApp!");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Ver catálogo de filmes");
+        System.out.println("2 - Ver catálogo de séries");
+        System.out.println("3 - Buscar Séries:");
+        System.out.println("4 - Ver perfil");
+        System.out.println("5 - Adicionar Filme");
+        System.out.println("6 - Adicionar Serie");
+        System.out.println("7 - Assistir");
+        System.out.println("0 - Sair");
+        System.out.print("\nSua opção: ");
+        int opcao = Integer.parseInt(scanner.nextLine());
+
+        return opcao;
+    }
+
+    public static void buscarSeries(PlataformaStreaming ps) {
+        System.out.println("Buscar Séries:");
+        System.out.println("Digite o nome do titulo:");
+        String audiovisual = scanner.next();
+        System.out.println(ps.buscarAudiovisual(audiovisual));
+    }
+
+    public static void adicionarFilme(PlataformaStreaming ps) {
+        System.out.println("Adicionar Filme...");
+        System.out.println("Digite o ID do filme");
+        int id = scanner.nextInt();
+        System.out.println("Digite o nome do filme:");
+        String nome = scanner.next();
+        System.out.println("Digite a data de lançamento:");
+        String anoLancamento = scanner.next();
+        System.out.println("Digite a duração do filme:");
+        int duracao = scanner.nextInt();
+        Filme filme = new Filme(id, nome, anoLancamento, duracao);
+        ps.adicionarFilme(filme);
+        ps.salvarFilme();
+    }
+
+    public static void adicionarSerie(PlataformaStreaming ps) {
+        System.out.println("Adicionar src.Serie...");
+        System.out.println("Digite o ID da serie");
+        int idSerie = scanner.nextInt();
+        System.out.println("Digite o nome da serie:");
+        String nomeSerie = scanner.next();
+        System.out.println("Digite a data de lançamento:");
+        String anoLancamentoSerie = scanner.next();
+        Serie serie = new Serie(idSerie, nomeSerie, anoLancamentoSerie);
+        ps.adicionarSerie(serie);
+        ps.salvarSerie();
+    }
+
+    public static void assistir(PlataformaStreaming ps, DadosService meusDados) {
+        System.out.println("Oque deseja ver ? 1 -> Filme 2 -> serie");
                     int opcAssistir = scanner.nextInt();
                     switch (opcAssistir) {
                         case 1:
@@ -93,10 +93,52 @@ public class App {
                         System.out.println("Avaliação cadastrada");
                     }
                     System.out.println("Obrigado.");
-                    break;
-                case 8:
+    }
+
+    public static void main(String[] args) {
+        PlataformaStreaming ps = new PlataformaStreaming();
+        DadosService meusDados = new DadosService(ps);
+        int opcao = -1;
+
+        do {
+
+            opcao = menu();
+
+            switch (opcao) {
+                case 0:
                     System.out.println("Saindo...");
-                    sair = true;
+                    break;
+                case 1:
+                    limparTela();
+                    System.out.println("Catálogo de filmes:");
+                    meusDados.carregarFilmes();
+                    break;
+                case 2:
+                    limparTela();
+                    System.out.println("Catálogo de séries:");
+                    meusDados.carregarSeries();
+                    break;
+                case 3:
+                limparTela();
+                    buscarSeries(ps);
+                    break;
+                case 4:
+                limparTela();
+                    System.out.println("Perfil:");
+                    meusDados.carregarClientes();
+                    break;
+                case 5:
+                limparTela();
+                    adicionarFilme(ps);
+                    break;
+
+                case 6:
+                limparTela();
+                    adicionarSerie(ps);
+                    break;
+                case 7:
+                limparTela();
+                    assistir(ps, meusDados);
                     break;
                 default:
                     System.out.println("Opção inválida!");
@@ -104,7 +146,8 @@ public class App {
             }
 
             System.out.println();
-        }
+            pausa();
+        } while (opcao != 0);
 
         scanner.close();
     }
